116e654d8ef81f3e27feab805db3323f9f35ae5f
Jonathan <web-praktik@jcd.dk>
2019-02-21 10:04:24 +0100

    Add project files.

---
+++ lift-messenger/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace lift_messenger
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
